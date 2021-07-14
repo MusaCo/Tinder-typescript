70de0a42a1faa305372ac2958caac32579c4f987
MusaCo <muslimxbox2@gmail.com>
2021-07-14 23:21:39 +0300

    initial committ

--- src/sidebar/Sidebar.tsx
+++ src/sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ export default function Sidebar() {
     return (
         <div className="sidebar" data-testid="sidebar">
             <div className="sidebarWrapper" data-testid="sidebarWrapper">
-        
+            
             </div>
         </div>
     )
